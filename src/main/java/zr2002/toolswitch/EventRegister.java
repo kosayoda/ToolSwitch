e4136c3aef20c8ada928a24baccfb4a7d84c9cd3
zr200222 <53227813+zr200222@users.noreply.github.com>
2020-03-01 21:25:32 -0500

    Update EventRegister.java

--- src/main/java/zr2002/toolswitch/EventRegister.java
+++ src/main/java/zr2002/toolswitch/EventRegister.java
@@ -1,4 +1,4 @@
-package zr2002.toolswitch;
+package com.zr2002.ToolSwitch;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,7 +11,7 @@ import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.client.event.ClientChatEvent;
-import net.minecraftforge.client.event.DrawBlockHighlightEvent;
+import net.minecraftforge.client.event.DrawHighlightEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class EventRegister {
@@ -21,6 +21,8 @@ public class EventRegister {
 	private static int fortune = 8;
 	private static int silk = 0;
 	private static int shovel = 1;
+	private static int axe = 2;
+	private static int shears = 3;
 	private static Minecraft mc = Minecraft.getInstance();
 	private static boolean isFluid;
 	private static String currentFluid;
@@ -39,11 +41,11 @@ public class EventRegister {
 			event.setCanceled(true);
 			if(toggle) {
 				toggle = false;
-				mc.player.sendMessage(new StringTextComponent("ง4ToolSwitch Disabled!"));
+				mc.player.sendMessage(new StringTextComponent("ยง4ToolSwitch Disabled!"));
 			}
 			else {
 				toggle = true;
-				mc.player.sendMessage(new StringTextComponent("ง4ToolSwitch Enabled!"));
+				mc.player.sendMessage(new StringTextComponent("ยง4ToolSwitch Enabled!"));
 			}
 		}
 		
@@ -51,7 +53,7 @@ public class EventRegister {
 			event.setCanceled(true);
 			if(message.substring(21).equals("1") || message.substring(21).equals("2") || message.substring(21).equals("3") || message.substring(21).equals("4") || message.substring(21).equals("5") || message.substring(21).equals("6") || message.substring(21).equals("7") || message.substring(21).equals("8") || message.substring(21).equals("9")) {
 				silk = Integer.parseInt(message.substring(21))-1;
-				mc.player.sendMessage(new StringTextComponent("ง4Silk pickaxe slot set to slot #" + (silk+1) + "!"));
+				mc.player.sendMessage(new StringTextComponent("ยง4Silk pickaxe slot set to slot #" + (silk+1) + "!"));
 			}
 		}
 		
@@ -59,7 +61,7 @@ public class EventRegister {
 			event.setCanceled(true);
 			if(message.substring(24).equals("1") || message.substring(24).equals("2") || message.substring(24).equals("3") || message.substring(24).equals("4") || message.substring(24).equals("5") || message.substring(24).equals("6") || message.substring(24).equals("7") || message.substring(24).equals("8") || message.substring(24).equals("9")) {
 				fortune = Integer.parseInt(message.substring(24))-1;
-				mc.player.sendMessage(new StringTextComponent("ง4Fortune pickaxe slot set to slot #" + (fortune+1) + "!"));
+				mc.player.sendMessage(new StringTextComponent("ยง4Fortune pickaxe slot set to slot #" + (fortune+1) + "!"));
 			}
 		}
 		
@@ -67,16 +69,32 @@ public class EventRegister {
 			event.setCanceled(true);
 			if(message.substring(23).equals("1") || message.substring(23).equals("2") || message.substring(23).equals("3") || message.substring(23).equals("4") || message.substring(23).equals("5") || message.substring(23).equals("6") || message.substring(23).equals("7") || message.substring(23).equals("8") || message.substring(23).equals("9")) {
 				shovel = Integer.parseInt(message.substring(23))-1;
-				mc.player.sendMessage(new StringTextComponent("ง4Shovel slot set to slot #" + (shovel+1) + "!"));
+				mc.player.sendMessage(new StringTextComponent("ยง4Shovel slot set to slot #" + (shovel+1) + "!"));
 			}
 		}
 		
+		if(message.contains("/toolswitch-set axe ")) {
+			event.setCanceled(true);
+			if(message.substring(20).equals("1") || message.substring(20).equals("2") || message.substring(20).equals("3") || message.substring(20).equals("4") || message.substring(20).equals("5") || message.substring(20).equals("6") || message.substring(20).equals("7") || message.substring(20).equals("8") || message.substring(20).equals("9")) {
+				shovel = Integer.parseInt(message.substring(20))-1;
+				mc.player.sendMessage(new StringTextComponent("ยง4Axe slot set to slot #" + (axe+1) + "!"));
+			
+			}
 		
+		}
+		
+		if(message.contains("/toolswitch-set shears ")) {
+			event.setCanceled(true);
+			if(message.substring(23).equals("1") || message.substring(23).equals("2") || message.substring(23).equals("3") || message.substring(23).equals("4") || message.substring(23).equals("5") || message.substring(23).equals("6") || message.substring(23).equals("7") || message.substring(23).equals("8") || message.substring(23).equals("9")) {
+				axe = Integer.parseInt(message.substring(23))-1;
+				mc.player.sendMessage(new StringTextComponent("ยง4Shears slot set to slot #" + (shears+1) + "!"));
+			}
+		}
 		
 	}
 	
 	@SubscribeEvent
-	public static void onHighlight(DrawBlockHighlightEvent event) {
+	public static void onHighlight(DrawHighlightEvent event) {
 		
 		
 		RayTraceResult ray = Minecraft.getInstance().getRenderViewEntity().func_213324_a(6.0D, 0.0F, false);
@@ -168,6 +186,28 @@ public class EventRegister {
 	            	if(currentBlock.contains("sand") && !currentBlock.contains("sandstone") && toggle) {
 		            	Minecraft.getInstance().player.inventory.currentItem = shovel;
 		            	}
+	            	if(currentBlock.contains("log") && toggle) {
+	            		Minecraft.getInstance().player.inventory.currentItem = axe;
+	            	}
+	            	
+	            	if(currentBlock.contains("wood") && toggle) {
+	            		Minecraft.getInstance().player.inventory.currentItem = axe;
+	            	}
+	            	
+	            	if(currentBlock.contains("plank") && toggle) {
+	            		Minecraft.getInstance().player.inventory.currentItem = axe;
+	            	}
+	            	
+	            	if(currentBlock.contains("chest") && toggle) {
+	            		Minecraft.getInstance().player.inventory.currentItem = axe;
+	            	}
+	            	
+	            	if(currentBlock.contains("leaves") && toggle) {
+	            		Minecraft.getInstance().player.inventory.currentItem = shears;
+	            	}
+	            	if(currentBlock.contains("wool") && toggle) {
+	            		Minecraft.getInstance().player.inventory.currentItem = shears;
+	            	}
 	            	
 	            }
 	            
